02db9bff11597f145762080563e4648f1dc57a5c
starkfire <plpantoja49@gmail.com>
2024-04-16 01:47:09 +0800

    feat: print_results parameter, return full_result

--- src/ensemble.py
+++ src/ensemble.py
@@ -19,7 +19,7 @@ class EnsembleClassifier(HaarCascadeClassifier):
                             "whiteface": (255, 0, 255)}
 
     # override
-    def classify(self, image, display=True, as_matlike=False):
+    def classify(self, image, display=True, as_matlike=False, print_results=True):
         """
         Runs the entire object detection + classification process.
         """
@@ -41,18 +41,19 @@ class EnsembleClassifier(HaarCascadeClassifier):
 
         haar_result = self.parse_result(bird_det, cockatiel_det)
 
+        snet_result = None
+
         # if the Haar classifier detects a cockatiel, run the SqueezeNet classifier
         if haar_result["label"] == "cockatiel":
-            snet_result = None
-
             # if the input image is not MatLike, pass the path to the image instead
             # of the processed 'frame'
             if not as_matlike:
-                snet_result = self.squeezenet_clf.test(image)
+                snet_result = self.squeezenet_clf.test(image, print_results=print_results)
             else:
-                snet_result = self.squeezenet_clf.test(frame, as_matlike=True)
+                snet_result = self.squeezenet_clf.test(frame, as_matlike=True, print_results=print_results)
 
-            print("[SqueezeNet Result] Label: {}, Probability: {}, Time: {}".format(snet_result["label"], snet_result["probability"], snet_result["inference_time"]))
+            if print_results:
+                print("[SqueezeNet Result] Label: {}, Probability: {}, Time: {}".format(snet_result["label"], snet_result["probability"], snet_result["inference_time"]))
             
             # use bounding boxes from Haar, and use label from SqueezeNet
             frame = self.attach_bounding_boxes(frame, haar_result["bboxes"], snet_result["label"])
@@ -68,7 +69,8 @@ class EnsembleClassifier(HaarCascadeClassifier):
 
         return {
             "frame": frame,
-            "result": result_label
+            "result": result_label,
+            "full_result": snet_result
         }
 
     # override
